3cf8557a2e6ae3c79ccac7231848dccebd01990d
Arthur Gomes <72204367+arthuzim@users.noreply.github.com>
2023-05-24 14:14:22 -0300

    Classe Department

---
+++ src/main/java/Application/main.java
@@ -0,0 +1,11 @@
+package Application;
+
+import Models.Department;
+
+public class main {
+    public static void main(String[] args) {
+        Department department = new Department("It security", 01);
+
+        System.out.println(department);
+    }
+}
---
+++ src/main/java/Models/Department.java
@@ -0,0 +1,29 @@
+package Models;
+
+public class Department {
+    private String name;
+    private Integer id;
+
+    public Department() {
+    }
+    public Department(String name, Integer id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
