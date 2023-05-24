5e293ff63ac6ab480e7e1f2d8837a28b43998e7c
Arthur Gomes <72204367+arthuzim@users.noreply.github.com>
2023-05-24 14:31:25 -0300

    Classe Seller

--- src/main/java/Application/main.java
+++ src/main/java/Application/main.java
@@ -1,11 +1,16 @@
 package Application;
 
 import Models.Department;
+import Models.Seller;
+
+import java.util.Date;
 
 public class main {
     public static void main(String[] args) {
         Department department = new Department("It security", 01);
-
         System.out.println(department);
+
+        Seller seller = new Seller(1, "Arthur", "ag@gmail.com", new Date(), 1000.0, department);
+        System.out.println(seller);
     }
 }
